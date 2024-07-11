7ca2f67af62f3d01daf259c14f5bebac1bd22000
marco-scocca <marcoscocca1991@gmail.com>
2024-07-11 08:46:20 +0200

    creo while di verifica

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@ function generateRandomNumber() {
     return Math.floor(Math.random() * 100 + 1);
 }
 
+function createArrayNumbers() {
+    let numbers = [];
+    while (numbers.length < 5) {
+        let num = gerateRandomNumber();
+        if (!numbers.includes()) {
+            numbers.push(num);
+        }
+    }
+}
+
