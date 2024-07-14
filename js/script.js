53a004e02496d74e702d46328195ce032bbded18
marco-scocca <marcoscocca1991@gmail.com>
2024-07-14 08:39:44 +0200

    mostro a video i numeri randomici

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
-function generateRandomNumber() {
+function genreateRandomNumber() {
     return Math.floor(Math.random() * 100 + 1);
 }
 
 function createArrayNumbers() {
     let numbers = [];
     while (numbers.length < 5) {
-        let num = gerateRandomNumber();
-        if (!numbers.includes()) {
+        let num = genreateRandomNumber();
+        if (!numbers.includes(num)) {
             numbers.push(num);
         }
     }
+    return numbers;
 }
+
+let random_numbers = createArrayNumbers();
+
+document.getElementById('numbers').innerHTML = random_numbers;
 
